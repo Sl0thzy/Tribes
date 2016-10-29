938692e1975b4b416680f4aff75dd0beebb3bbf5
Gavin <gavin.dumont01@gmail.com>
2016-10-29 12:42:00 -0400

    Add files via upload

---
+++ Tribes/Assets/Scripts/Weapons/ArrowSys.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Rigidbody))]
+public class ArrowSys : MonoBehaviour {
+
+	Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		transform.LookAt (transform.position + rb.velocity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt (transform.position + rb.velocity);
+	}
+}
---
+++ Tribes/Assets/Scripts/Weapons/PhysShootSys.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhysShootSys : MonoBehaviour {
+
+	//private Player player;
+	public float force;
+	public float maxForce;
+	public float forceIncr;
+
+	private Camera camera;
+	public GameObject projectile;
+
+	public int ammo;
+	private bool canShoot = true;
+
+	public float dmg;
+
+
+	private void Start(){
+		force = 0;
+		camera = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Camera> ();
+		//maxForce = 5;
+		// ammo = 12;
+	}
+
+	private void Update(){
+		if(projectile != null){
+			Debug.Log (canShoot);
+		if (canShoot) {
+				if (Input.GetKey (KeyCode.Mouse0)  && ammo > 0) {
+					if (force < maxForce) {
+						force += forceIncr;
+					}
+				}
+					
+				if(Input.GetKeyUp(KeyCode.Mouse0) && force > 0){
+					Debug.Log ("Fire");
+					GameObject proj = Instantiate(projectile, transform.position,Quaternion.identity) as GameObject;
+					proj.transform.localRotation = Quaternion.Euler (90, 0, 0);	
+					proj.GetComponent<Rigidbody>().AddForce(transform.forward * force);
+					
+					force = 0;
+					ammo -=1;
+				}
+			}
+		}
+		}
+	}
